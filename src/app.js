c70f803fbdf3b71c530291b535dd8715d43ea399
Fernando Cellini <fernandocellini100@gmail.com>
2024-02-12 17:27:54 +0000

    added routes

--- src/app.js
+++ src/app.js
@@ -1,27 +1,12 @@
 const express = require('express');
-
+const userRoutes = require('./interfaces/routes/userRoutes');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Simple logging middleware for every request
-app.use((req, _res, next) => {
-  console.log(`${new Date()}: ${req.method} ${req.path}`);
-  next();
-});
-
-app.get('/', async (req, res) => {
-  try {
-    const addUserUseCase = new AddUserUseCase(userRepositoryLocal);
-    const user = await addUserUseCase.execute(req.body);
-
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
